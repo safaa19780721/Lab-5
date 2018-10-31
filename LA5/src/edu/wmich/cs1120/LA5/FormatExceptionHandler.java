0a1b1dddcc5ee15ba45083594b4495f883cae43d
Michael <Michael@LAPTOP-FNO4A9K3>
2018-10-31 12:02:28 -0400

    Michael Coffey eliminated erros

--- LA5/src/edu/wmich/cs1120/LA5/FormatExceptionHandler.java
+++ LA5/src/edu/wmich/cs1120/LA5/FormatExceptionHandler.java
@@ -4,28 +4,16 @@ import java.io.FileNotFoundException;
 
 public class FormatExceptionHandler implements IFormatExceptionHandler {
 
-	@Override
 	public void handleFileNotFoundException(FileNotFoundException e) {
-		// TODO Auto-generated method stub
-
+		
 	}
-	
-	@Override
-	public void handlePhoneNumberFormatException(FileNotFoundException e) {
-		// TODO Auto-generated method stub
-
+	public void handlePhoneNumberFormatException(PhoneNumberFormatException e) {
+		
 	}
-
-	@Override
 	public void handleEmailFormatException(EmailAddressFormatException e) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
-	@Override
 	public void handleNameFormatException(NameFormatException e) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
