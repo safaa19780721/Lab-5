b5f666dd7d37a2839dbbfd5c6353e2f0eb912c5c
Michael <Michael@LAPTOP-FNO4A9K3>
2018-10-31 11:44:44 -0400

    Restart

--- LA5/src/edu/wmich/cs1120/LA5/Main.java
+++ LA5/src/edu/wmich/cs1120/LA5/Main.java
@@ -3,11 +3,7 @@ package edu.wmich.cs1120.LA5;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hey Michael I love you");
-		System.out.println("Hey Dylan I am pissed");
-		System.out.println("Someone is pissing me off");
-		System.out.println("He is doing this literally the hardest way of all time");
-		System.out.println("True");
+		
 	}
 
 }
