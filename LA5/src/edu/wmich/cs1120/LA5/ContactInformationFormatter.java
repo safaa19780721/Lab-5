58a5f06c662d2545d51110b530193d38cdee3379
Michael <Michael@LAPTOP-FNO4A9K3>
2018-10-31 12:00:13 -0400

    Michael Coffey implemented methods from interface

--- LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
+++ LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
@@ -2,4 +2,28 @@ package edu.wmich.cs1120.LA5;
 
 public class ContactInformationFormatter implements IContactInformationFormatter {
 
+	@Override
+	public void readContactInformation(String[] filePaths) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void formatContactInformation(String fileName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void formatEmail(String email) throws EmailAddressFormatException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void formatPhoneNumber(String phoneNumber) throws PhoneNumberFormatException {
+		// TODO Auto-generated method stub
+
+	}
+
 }
