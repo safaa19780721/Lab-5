18cb7ffd6d04929f4d20f35c2f644abe12b79dc3
Dylan <Dylan@10.80.141.22>
2018-10-31 11:34:43 -0400

    Dylan Tarnacki added a method

--- LA5/src/edu/wmich/cs1120/LA5/TestDylansSexuality.java
+++ LA5/src/edu/wmich/cs1120/LA5/TestDylansSexuality.java
@@ -3,8 +3,13 @@ package edu.wmich.cs1120.LA5;
 public class TestDylansSexuality {
 
 	private String sexuality = "Straight kinda";
+	private String michaelsSexuality = "Pretty Gay";
 	
 	public String getSexuality() {
 		return sexuality;
+	}
+	
+	public String getMichaelsSexuality() {
+		return michaelsSexuality;
 	}
 }
