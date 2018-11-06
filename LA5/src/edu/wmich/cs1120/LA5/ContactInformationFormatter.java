97bc7b279f2ee282f9ea1fd4fd980cb38fad7486
Michael <Michael@LAPTOP-FNO4A9K3>
2018-11-06 11:31:01 -0500

    Michael Coffey ContactInfomationFormatter

--- LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
+++ LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
@@ -1,17 +1,56 @@
 package edu.wmich.cs1120.LA5;
+import java.util.Scanner;
+import java.io.File;
+import java.util.*;
 
 public class ContactInformationFormatter implements IContactInformationFormatter {
 
-	@Override
+	private Scanner in;
+	private String emailLine, firstLetter, phoneDigits = "";
+	private ArrayList<String> names, firstName, lastName, phoneNumber, emails;
+	
 	public void readContactInformation(String[] filePaths) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < filePaths.length;i++) {
+			in = new Scanner(filePaths[i]);
+			while(in.hasNext()) {
+				firstName.add(in.next());
+				lastName.add(in.next());
+				phoneNumber.add(in.nextLine());
+				emails.add(in.nextLine());
+			}
+		}
+		
 	}
 
-	@Override
 	public void formatContactInformation(String fileName) {
-		// TODO Auto-generated method stub
-
+		for(int i = 0; i < firstName.size(); i++) {
+			//Formatting Name
+			firstName.get(i).toUpperCase();
+			firstLetter = firstName.get(i).substring(0, 1);
+			firstLetter.toUpperCase();
+			firstName.set(i, (firstLetter + firstName.get(i).substring(1)));
+			
+			lastName.get(i).toUpperCase();
+			firstLetter = lastName.get(i).substring(0, 1);
+			firstLetter.toUpperCase();
+			lastName.set(i, (firstLetter + lastName.get(i).substring(1)));
+			
+			names.set(i, firstName.get(i) + lastName.get(i));
+			
+			//Formatting Phone Number
+			for(int x = 0; x < phoneNumber.get(i).length(); x++) {
+				try {
+					Integer.parseInt(phoneNumber.get(i));
+					phoneDigits = phoneDigits + phoneNumber.get(i).charAt(x); 
+				} catch(NumberFormatException E) {
+				} catch(NullPointerException E) {
+				}
+			}
+			phoneNumber.set(i,("("+ phoneDigits.substring(0,3)) + ")" + phoneDigits.substring(3,7) + "-" + phoneDigits.substring(7));
+			
+			//Formatting Email
+			
+		}
 	}
 
 	@Override
--- LA5/src/edu/wmich/cs1120/LA5/Main.java
+++ LA5/src/edu/wmich/cs1120/LA5/Main.java
@@ -1,9 +1,12 @@
 package edu.wmich.cs1120.LA5;
+import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		}
+		
 	}
-
-}
